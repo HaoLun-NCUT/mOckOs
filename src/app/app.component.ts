3df3ce59c09696b3281954d58166f82d2b87afa5
at home <9b117201@gm.student.ncut.edu.tw>
2025-04-03 18:36:15 +0800

    fu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [],
+  template: `{{title}}`,
+  styles: ``
 })
 export class AppComponent {
   title = 'mOckOs';
