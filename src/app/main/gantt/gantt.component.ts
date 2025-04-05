c4c04332e03dcff87765d06e473d9aef0c222072
at home <9b117201@gm.student.ncut.edu.tw>
2025-04-06 01:29:28 +0800

    fix

--- src/app/main/gantt/gantt.component.ts
+++ src/app/main/gantt/gantt.component.ts
@@ -4,6 +4,16 @@ import { Component } from '@angular/core';
   selector: 'app-gantt',
   imports: [],
   templateUrl: './gantt.component.html',
+  styles: [`
+   :host {
+    width: 100%;
+    height: 100%;
+    background: url('public/work-in-progress.png') no-repeat center center; /* 背景圖片 */
+    background-size: auto 50%; /* 縮小背景圖片 */
+    background-blend-mode: multiply; /* 混合模式，讓圖片與半透明背景結合 */
+    filter: opacity(50%); /* 設置圖片透明度為 50% */
+  }`
+  ]
 })
 export class GanttComponent {
 
